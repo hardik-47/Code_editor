47fac1a483f51a4d69d6ca26abbe4eedd8d81bdb
hardik-47 <jainhardik929@gmail.com>
2025-07-15 15:13:41 +0530

    port

--- server.js
+++ server.js
@@ -65,4 +65,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+server.listen(PORT, '0.0.0.0', () => console.log(`Listening on ${PORT}`));
